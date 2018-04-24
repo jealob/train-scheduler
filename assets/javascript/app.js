2afa231b0a2138e30a3754e756445da1d264e596
jealob <jealob@gmail.com>
2018-04-24 17:22:56 -0500

    creates basic html and css files

---
+++ assets/javascript/app.js
@@ -0,0 +1,11 @@
+// JavaScript  document
+
+// *****************************************//
+// This simple JS code is applying firebase and moment.js technology to build a train scheduler application.
+// ***************************************//
+
+// Document Ready
+$(document).ready(function () {
+    // Declare global variables
+   
+})
